8767c0219ef40ed4877a8c40fab7f24089c41a4f
Bendi11 <bkliebmann@gmail.com>
2021-02-26 12:14:53 -0800

    Updated GUI to Dear ImGui

--- src/include/gui.hpp
+++ src/include/gui.hpp
@@ -1,37 +1,16 @@
+#pragma once
 
-#include <wx/wxprec.h>
-
-#ifndef WX_PRECOMP
-#include <wx/wx.h>
-#endif
-
-#include <wx/wxhtml.h>
-#include <wx/fs_inet.h>
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+#include "glad/glad.h"
 
 /**
- * @brief Class that wxWidgets implements, opens a wxFrame to draw GUI to
+ * @brief Class that contains all methods for displaying RSS management
+ * and viewing GUI
  * 
  */
-class RssApp : public wxApp
+class RssView
 {
-public:
-    virtual bool OnInit(); 
 
-private:
-
-};
-
-/**
- * @brief Class that implements all GUI elements
- * 
- */
-class RssFrame : public wxFrame
-{
-public:
-    RssFrame(void);
-
-private:
-
-    void OnExit(wxCommandEvent& e);
-    
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
+#include "include/gui.hpp"
+
 #define LOG_IMPL
 #include "include/logger.hpp"
-#include "include/gui.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-
-wxIMPLEMENT_APP(RssApp);
-IMPLEMENT_WX_THEME_SUPPORT;
